cc130e97a12dfdbf80a9b3fe5d23c530bb76203c
unknown <zainalimalik295@gmail.com>
2026-05-04 01:28:35 -0700

    Fix Express 5 catch-all route syntax

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use('/api/notices', noticesRoutes);
 
 // Serve frontend in production
 app.use(express.static(path.join(__dirname, '../dist')));
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
